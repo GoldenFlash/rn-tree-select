242d4df15692ba296d38261d33778091bbd8a842
GoldenFlash <892458829@qq.com>
2019-06-29 17:15:54 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # rn-tree-select
+## install
+![The final rendering](./rn-tree-select.png)
+
+$ npm install native-echarts --save 或 $ yarn add rn-tree-select 
+
+## Usage
+  ```
+  import React, {Component} from 'react';
+  import {View} from 'react-native';
+  import Tree from "rn-tree-select"
+
+  type Props = {};
+  export default class App extends Component<Props> {
+    render() {
+      let data = [
+        { "id": 1000, "name": 1000 },
+        { "id": 1100, "name": 1100, "parentId": 1000 },
+        { "id": 1110, "name": 1110, "parentId": 1100 },
+        { "id": 1120, "name": 1120, "parentId": 1100 },
+        { "id": 1121, "name": 1121, "parentId": 1120 },
+        { "id": 1122, "name": 1122, "parentId": 1120 },
+        { "id": 1200, "name": 1200, "parentId": 1000 },
+        { "id": 1210, "name": 1210, "parentId": 1200 },
+        { "id": 1300, "name": 1300, "parentId": 1000 },
+        { "id": 2000, "name": 2000 },
+        { "id": 2100, "name": 2100, "parentId": 2000 },
+        { "id": 2200, "name": 2200, "parentId": 2000 }
+      ]
+      return (
+        <View style={styles.container}>
+          <Tree 
+            style={styles.tree} 
+            itemStyle={{marginTop:5}}
+            textStyle={{color:"#333"}}
+            data={data}
+            onChange={(selectArr)=>{
+              console.log("selectArr",selectArr)
+            }}
+            >
+          </Tree>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    }
+  })
+  ```
++ 选项列表
+  ```
+  let data = [
+      { "id": 1000, "name": 1000 },
+      { "id": 1100, "name": 1100, "parentId": 1000 },
+      { "id": 1110, "name": 1110, "parentId": 1100 },
+      { "id": 1120, "name": 1120, "parentId": 1100 },
+      { "id": 1121, "name": 1121, "parentId": 1120 },
+      { "id": 1122, "name": 1122, "parentId": 1120 },
+      { "id": 1200, "name": 1200, "parentId": 1000 },
+      { "id": 1210, "name": 1210, "parentId": 1200 },
+      { "id": 1300, "name": 1300, "parentId": 1000 },
+      { "id": 2000, "name": 2000 },
+      { "id": 2100, "name": 2100, "parentId": 2000 },
+      { "id": 2200, "name": 2200, "parentId": 2000 }
+    ]
+    //id为每一项的唯一标识
+    //parentId 代表此项为id 等于parentId 的项的子项
+  ```    
+
++ 获取选中数据
+  - 可通过 onChange 事件获取
+  ```
+  onChange={(selectArr)=>{
+     console.log("selectArr",selectArr)
+  }}
+  ```
+- 通过ref获取
+
+  ```
+    let valueArr = this.refs.treeSelect.getCheckedValue()
+  ```
+
+
+
+## props
+
+|Props|Explain|type|require|default|          
+|:-------|:--------|:--------|:-------|:----------|
+|data|数据，树结构数据|array|yes|[]|    
+|onChange|勾选改变触发事件，返回选中节点组成的数组|function|no|no|  
+|style|指定组件的整体样式|style|no|{}|
+|itemStyle|指定item项的样式|style|no|{}|
+|textStyle|指定每一项文字的样式|style|no|{}|
+
+## methods
+ - getCheckedValue
+ 可获取选中项组成的数组
+  ```
+    let valueArr = this.refs.treeSelect.getCheckedValue(）
+   ```
--- component/index.js
+++ component/index.js
@@ -1,51 +1,219 @@
-import React,{Component} from "react"
-import { StyleSheet, View, TextInput, FlatList, Text, TouchableOpacity } from 'react-native';
-let list = [
-    { "id": 1000, "name": 1000 },
-    { "id": 1100, "name": 1100, "parentId": 1000 },
-    { "id": 1110, "name": 1110, "parentId": 1100 },
-    { "id": 1120, "name": 1120, "parentId": 1100 },
-    { "id": 1121, "name": 1121, "parentId": 1120 },
-    { "id": 1122, "name": 1122, "parentId": 1120 },
-    { "id": 1200, "name": 1200, "parentId": 1000 },
-    { "id": 1210, "name": 1210, "parentId": 1200 },
-    { "id": 1300, "name": 1300, "parentId": 1000 },
-    { "id": 2000, "name": 2000 },
-    { "id": 2100, "name": 2100, "parentId": 2000 },
-    { "id": 2200, "name": 2200, "parentId": 2000 }
-]
-export default class Rntree extends Component{
-    constructor(props){
+import React, { Component } from "react"
+import { StyleSheet, View, Image, FlatList, Text, TouchableOpacity } from 'react-native';
+
+export default class Rntree extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            treeData: this.initTreeData(list)
+        this.state = {
+            treeData: this.initTreeData(JSON.parse(JSON.stringify(props.data||[])))
         }
     }
-    initTreeData=()=>{
-        let tree = {}
-        let list = this.state.list
-        list.forEach((item)=>{
-            let id = item.id
-            let parentId = item.parentId
-            if(!tree[id]){
-                item.expend= false
-                item.checked = false
-                tree[id] = item
-            }
-            if(parentId){
-                tree[parentId].children =[] 
-                tree[parentId].children.push(item)
+    onChange = () => {
+        if (this.props.onChange) {
+            let output = this.getCheckedValue()
+            this.props.onChange(output)
+        }
+    }
+    getCheckedValue = () => {
+        let treeData = this.state.treeData
+        let data = JSON.parse(JSON.stringify(treeData))
+        let keys = Object.keys(treeData)
+        let output = []
+        keys.filter((key) => {
+            let item = data[key]
+            if (item.checked) {
+                delete item.children
+                delete item.isRoot
+                delete item.expend
+                delete item.checked
+                delete item.hasChildrenChecked
+                output.push(item)
             }
         })
-        console.log("treeData", treeData)
+        return output
+    }
+    initTreeData = (data) => {
+        let tree = {}
+        data.forEach((item) => {
+            let id = item.id && item.id.toString()
+            let parentId = item.parentId && item.parentId.toString()
+
+            item.expend = false
+            item.checked = false
+
+            if (parentId) {
+                if (tree[parentId].children) {
+                    tree[parentId].children.push(item)
+                } else {
+                    tree[parentId].children = []
+                    tree[parentId].children.push(item)
+                }
+            }
+            if (!parentId) {
+                item.isRoot = true
+            }
+
+            if (!tree[id]) {
+                tree[id] = item
+            } else {
+                tree[id] = {
+                    ...item,
+                    ...tree[id]
+                }
+            }
+        })
         return tree
+    }
+    toggleFold = (node) => {
+        let treeData = this.state.treeData
+        let nodeId = node.id
+        if (!nodeId) return
+        treeData[nodeId].expend = !node.expend
+        this.setState({
+            treeData
+        })
+    }
+    toggleCheck = (node) => {
+        let treeData = this.state.treeData
+        let nodeId = node.id
+        let parentId = node.parentId
+        if (!nodeId) return
+        treeData[nodeId].checked = !node.checked
+        treeData[nodeId].hasChildrenChecked = false
+        if (node.children && node.children.length) {
+            this.checkSub(treeData, node)
+        }
+        if (parentId && treeData[parentId]) {
+            this.checkParent(treeData, node)
+        }
+        this.onChange(treeData)
+        this.setState({
+            treeData
+        })
+    }
+    checkParent = (treeData, node) => {
+        let parentId = node.parentId
+        let parentNode = treeData[parentId]
+        if (parentNode.children && parentNode.children.length) {
+            let parentChecked = parentNode.children.every((item) => {
+                return item.checked
+            })
+            let hasChildrenChecked = parentNode.children.some((item) => {
+                return item.checked || item.hasChildrenChecked
+            })
+            parentNode.hasChildrenChecked = hasChildrenChecked
+            parentNode.checked = parentChecked
+        }
+        if (parentNode.parentId) {
+            this.checkParent(treeData, parentNode)
+        }
+    }
+    checkSub = (treeData, node) => {
+        let nodeId = node.id
+        let checked = node.checked
+        let children = treeData[nodeId].children
+        children.forEach((item) => {
+            item.checked = checked
+            item.hasChildrenChecked = false
+            if (item.children && item.children.length) {
+                this.checkSub(treeData, item)
+            }
+        })
 
     }
-    render(){
-        return(
+    _renderItem = ({ item }) => {
+        return (
             <View>
+                <View style={styles.items}>
+                    {
+                        item.children && item.children.length > 0 ?
+                            <TouchableOpacity
+                                onPress={() => {
+                                    this.toggleFold(item)
+                                }}
+                            >
+                                <Image
+                                    resizeMode="contain"
+                                    style={styles.image}
+                                    source={
+                                        item.expend ?
+                                            require("../icon/arrow-down-b.png") :
+                                            require("../icon/arrow-right-b.png")}
+                                ></Image>
+                            </TouchableOpacity>
+                            : null
+                    }
+                    <TouchableOpacity
+                        style={item.children ? null : { marginLeft: 20 }}
+                        onPress={() => {
+                            this.toggleCheck(item)
+                        }}>
+                        <Image
+                            style={styles.image}
+                            resizeMode="contain"
+                            source={
+                                item.checked ?
+                                    require("../icon/checkbox-marked.png") :
+                                    item.hasChildrenChecked ?
+                                        require("../icon/checkbox-indeterminate-fill.png") :
+                                        require("../icon/check_box_outline_blank.png")
+                            }
+                        ></Image>
+                    </TouchableOpacity>
+                    <Text style={props.textStyle}>{item.name}</Text>
+                </View>
+                {
+                    item.expend && item.children && item.children.length > 0 ?
+                        this._renderBranch(item.children)
+                        : null
+                }
+            </View>
+        )
 
+    }
+    _renderBranch(data) {
+        return (
+            <View style={styles.branch_wrapper}>
+                <FlatList
+                    data={data}
+                    extraData={this.state}
+                    keyExtractor={(item, index) => "" + item.id + index}
+                    renderItem={this._renderItem}
+                />
+            </View>
+        )
+    }
+    render() {
+        let treeData = this.state.treeData
+        let key = Object.keys(treeData)
+        let root = []
+        key.forEach(item => {
+            if (treeData[item].isRoot) {
+                root.push(treeData[item])
+            }
+        })
+
+        let { style } = this.props
+        return (
+            <View style={style}>
+                {this._renderBranch(root)}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    items: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 5
+    },
+    image: {
+        width: 20,
+        height: 20
+    },
+    branch_wrapper: {
+        marginLeft: 20,
+        justifyContent: "flex-start"
+    }
+})
